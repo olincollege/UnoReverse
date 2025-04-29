ef44705f7cf38cd16c273244257a1b849206e035
alex_zigs <azeigler@olin.edu>
2025-04-29 11:55:02 -0400

    updating game and controller files

--- uno_game.py
+++ uno_game.py
@@ -1,4 +1,28 @@
-import uno_controller as UNOController
-from Source import uno_model as UNOModel
 
-pass
+import pygame
+from Source.uno_model import UNOGAMEMODEL
+from uno_controller import MouseController
+from uno_view import View
+
+
+#setup for the event
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock() #internal clock. May ve visualized in View
+model = UNOGAMEMODEL
+controller = MouseController(model)
+view = View(model)
+RUNNING = True #is the game running?
+DT = 0 #start time
+pygame.QUIT = RUNNING is False
+
+
+#everything should be in the "running" loop
+while RUNNING:
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            RUNNING = False
+        controller.handle_mouse_event(event)
+
+    #runs frame by frame, so you don't see cards in the background
+    screen.fill("blue")
