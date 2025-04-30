7017a2cdcf1cb08ff8dc85039acc84cd75a19b15
Lexi Greenberg <agreenberg2@olin.edu>
2025-04-29 22:16:00 -0400

    Updating model code after debugging

--- Source/uno_model.py
+++ Source/uno_model.py
@@ -1,7 +1,6 @@
-"""_summary_
-
-Returns:
-        _type_: _description_
+"""
+The model portion of the UNO game which saves the rules, the game operations,
+the player details, the card details
 """
 
 from random import shuffle
@@ -10,15 +9,30 @@ import time
 
 class PlayerDetails:
     """
-    Saves the neaaaccary information that the players need for an UNO game
+    Saves the nessaccary information that the players need for an UNO game
     """
 
     def __init__(self, personal_cards):
-        """ """
+        """
+        Initiliazes all the relevent details a player in an UNO game needs, including their hand,
+        if they called UNO or UNO OUT, and whether or not it is their turn
+
+        Args:
+            personal_cards: A list type representing the personal hand of cards for the
+            human interactive player in the UNO game
+        """
         self.hand = personal_cards
         self.uno = False
         self.unoout = False
         self.my_turn = True
+
+    def declare_win(self):
+        """
+        Prints a statement declaring the player has won if they have no more cards
+        and called UNO and UNO OUT
+        """
+        if self.hand == 0 and self.uno and self.unoout:
+            print("You win!!!")
 
     def __repr__(self):
         pass
@@ -30,34 +44,27 @@ class ComputerDetails:
     """
 
     def __init__(self, personal_cards):
-        """ """
+        """
+        Initiliazes all the relevent details a player in an UNO game needs, including their hand,
+        if they called UNO or UNO OUT, and whether or not it is their turn
+
+        Args:
+            personal_cards: A list type representing the personal hand of cards for the
+            virtual player in the UNO game
+
+        """
         self.hand = personal_cards
         self.uno = False
         self.unoout = False
         self.my_turn = False
 
-        self.rules = UNOGAMEMODEL()
-
-    def computer_choice(self, maindeck, hand):
+    def declare_win(self):
         """
-        Performs the artificial player's (computer's) turn by having them choose
-        what card they will play or if they will pick a card.
-
-        Returns:
-            The position number of the first eligiable card in the computer's
-            hand to be played or the picked a card statement.
+        Prints a statement declaring the virtual player has won if they have no more cards
+        and called UNO and UNO OUT
         """
-        for card_pos in enumerate(hand):
-            if self.rules.is_card_valid_move(hand[card_pos]):
-
-                maindeck.played_cards.append(hand[card_pos])
-                hand.pop(card_pos)
-                break
-
-            if card_pos == (len(hand) - 1) and not self.rules.is_card_valid_move(
-                hand[card_pos]
-            ):
-                self.rules.pick_a_card(hand)
+        if self.hand == 0 and self.uno and self.unoout:
+            print("The computer wins!!!")
 
     def __repr__(self):
         pass
@@ -69,12 +76,10 @@ class Card:
     sub function to cards that have special effects
     """
 
-    def __init__(self, suit, value, computer_class, player_class):
+    def __init__(self, suit, value):
         self.suit = suit
         self.value = value
         self.function = None
-        self.computer = computer_class
-        self.human = player_class
 
     def _wild_effect(self, wild_card):
         """
@@ -82,12 +87,14 @@ class Card:
 
         Args:
             wild_card: An instance of the Card class where self.function is wildcardeffect,
-                    representing a wild card from an UNO deck
+            representing a wild card from an UNO deck
         """
         new_color = input("Choose a new color base")
         wild_card.suit = new_color
 
-    def _plus_card_effect(self, plus_card, current_deck):
+    def _plus_card_effect(
+        self, plus_card, current_deck, computer_instance, player_instance
+    ):
         """
         Triggers the effects of a plus card in UNO by adding cards to the hand of the
         opposite player to place the plus card
@@ -97,46 +104,46 @@ class Card:
             current_deck: A list type representing the current state of the deck in the UNO game
         """
         plus_num = plus_card.value
-        if self.computer.my_turn is False:
-            self.computer.deck.append(current_deck[0:plus_num])
+        if computer_instance.my_turn is False:
+            computer_instance.hand.append(current_deck[0:plus_num])
             del current_deck[0:plus_num]
 
-        elif self.human.my_turn is False:
-            self.human.deck.append(current_deck[0:plus_num])
+        elif player_instance.my_turn is False:
+            player_instance.hand.append(current_deck[0:plus_num])
             del current_deck[0:plus_num]
 
-    def _skip_effect(self):
-        """
-        Skips the next player's turn, giving the current player another turn
-        """
-
-        if self.computer.my_turn is True:
-            self.computer.computer_choice()
-
-    def special_card_effect(self, placed_card, main_deck_instance):
+    def special_card_effect(
+        self, placed_card, main_deck_instance, computer_instance, player_instance
+    ):
         """
         Calls on the functions of special action cards in the UNO deck
 
         Args:
             placed_card: A class instant representing a card played in the UNO game
+
+            main_deck_instance: A class instant representing the current state of the
+            main deck of the UNO game
+
+            computer_instance: A class instant representing the current version of a state of a
+
+            player_instance:
         """
         match placed_card.function:
             case "wildcard":
                 self._wild_effect(placed_card)
 
             case "pluscard":
-                self._plus_card_effect(placed_card, main_deck_instance)
-
-            case "skipeffect":
-                self._skip_effect()
+                self._plus_card_effect(
+                    placed_card, main_deck_instance, computer_instance, player_instance
+                )
 
     def __repr__(self):
-        pass
+        return f"this card is a {self.suit} {self.value}"
 
 
 class Deck:
     """
-    _summary_
+    Builds all the decks used a UNO game
 
     """
 
@@ -145,31 +152,32 @@ class Deck:
         Initilizes the main UNO deck with all the most standard UNO cards, the personal deck's
         for the human and computer players, and the deck for cards that have been played in the game
 
+        Args:
+            original_deck: A list type that represents the main eck of cards from the UNO game
+            containing all the UNO cards at the start before distribution and play
         """
         self.main_deck = original_deck
 
         for color in ["red", "yellow", "green", "blue"]:
-            self.main_deck.append(
-                Card(color, i, ComputerDetails, PlayerDetails) for i in range(1, 10)
-            )
+            self.main_deck.extend([Card(color, i) for i in range(0, 10)])
 
-        wild1 = Card("wild", 0, ComputerDetails, PlayerDetails)
+        wild1 = Card("wild", 10)
         wild1.function = "wildcard"
         self.main_deck.append(wild1)
 
-        redplus2 = Card("red", 2, ComputerDetails, PlayerDetails)
+        redplus2 = Card("red", 2)
         redplus2.function = "pluscard"
         self.main_deck.append(redplus2)
 
-        yellowplus2 = Card("yellow", 2, ComputerDetails, PlayerDetails)
+        yellowplus2 = Card("yellow", 2)
         yellowplus2.function = "pluscard"
         self.main_deck.append(yellowplus2)
 
-        greenplus2 = Card("green", 2, ComputerDetails, PlayerDetails)
+        greenplus2 = Card("green", 2)
         greenplus2.function = "pluscard"
         self.main_deck.append(greenplus2)
 
-        blueplus2 = Card("blue", 2, ComputerDetails, PlayerDetails)
+        blueplus2 = Card("blue", 2)
         blueplus2.function = "pluscard"
         self.main_deck.append(blueplus2)
 
@@ -189,21 +197,39 @@ class Deck:
 
         shuffle(self.main_deck)
 
-        self.human_deck.hand.append(self.main_deck[0:6])
+        print(self.main_deck)
+
+        self.human_deck.hand.extend(self.main_deck[0:7])
         del self.main_deck[0:6]
 
-        self.computer_deck.hand.append(self.main_deck[0:6])
+        self.computer_deck.hand.extend(self.main_deck[0:7])
         del self.main_deck[0:6]
 
         position = 0
         start_card = self.main_deck[position]
 
-        while start_card.value in (0, +2) or start_card.suit == "wild":
+        while start_card.function is not None:
             position += 1
             start_card = self.main_deck[position]
 
         self.played_cards.append(start_card)
         self.main_deck.pop(position)
+
+    def refill(self):
+        """
+        Refills and reshuffles the main deck when it runs out during an UNO game by taking all
+        but the top played cards from the played card deck, shuffling them, and making them the
+        new main deck
+
+        """
+
+        if len(self.main_deck) == 0:
+            length_of_played_deck = len(self.played_cards)
+            temp_list = self.played_cards[0 : length_of_played_deck - 2]
+            self.played_cards = self.played_cards[-1]
+
+            shuffle(temp_list)
+            self.main_deck = temp_list
 
     def __repr__(self):
         pass
@@ -214,10 +240,17 @@ class UNOGAMEMODEL:
     Sets up the rules, game operation and winning conditions of a game of UNO
     """
 
-    def __init__(self):
-        """ """
+    def __init__(self, input_deck):
+        """
+        Sets up the initial decks of the UNO game by calling on the Deck class to get a
+        main class, played cards class, and the individual hands of each player
 
-        self.deck = Deck([])
+        Args:
+            input_deck: An instant of a class reprsenting the decks types for the start of the game
+
+        """
+
+        self.deck = Deck(input_deck)
 
         self.deck.shuffle_and_distribute()
 
@@ -225,21 +258,16 @@ class UNOGAMEMODEL:
 
         self.player_turn = self.deck.human_deck.my_turn
 
-        self.player_uno = self.deck.human_deck.uno
-
-        self.player_unoout = self.deck.human_deck.unoout
-
         self.computer_hand = self.deck.computer_deck.hand
 
         self.computer_turn = self.deck.computer_deck.my_turn
 
-        self.computer_uno = self.deck.computer_deck.uno
-
-        self.computer_unoout = self.deck.computer_deck.unoout
-
     def _flip_next_move(self):
         """
         Change which player's turn it is to make a move.
+
+        Return:
+            Return the sub class of the player whose current turn it is
         """
         if self.player_turn is True:
             self.computer_turn = True
@@ -255,14 +283,14 @@ class UNOGAMEMODEL:
         the number of the last played card, with the expection of wild cards.
 
         Args:
-            card: A tuple type representing the card picked to be played next
-            top_of_deck: A tuple type representing the last card played
+            card: An intant of a class representing the card picked to be played next
 
         Returns:
             Returns true if the card picked to be played is wild, or matches the color
             or number of the last card played. Return False otherwsie
         """
-        top_of_deck = self.deck.played_cards[(len(self.deck.played_cards) - 1)]
+        top_of_deck = self.deck.played_cards[-1]
+        print(f"a {top_of_deck.value} {top_of_deck.suit} card")
 
         if (
             (card.suit == top_of_deck.suit)
@@ -278,25 +306,29 @@ class UNOGAMEMODEL:
         Performs the game action of picking a card in the UNO game
 
         Args:
-            personal_hand: a list representing the player's hand who is currently picking a card
+            personal_hand: a list within a class instance representing the player's
+            hand who is currently picking a card
         """
 
         personal_hand.append(self.deck.main_deck[0])
         del self.deck.main_deck[0]
-        print("Picked A Card")
 
-    def check_for_winner(self, player):
+    def check_for_winner(self, player_instant):
         """
         Checks if a player has won the game
 
         Args:
-            player: A list type representing the hand of a player
+            player_intant: A intant of a class type representing a player in the game
 
         Returns:
             True if the specific player has won and False otherwise
         """
 
-        if len(player) == 0 and self.call_uno_out:
+        if (
+            len(player_instant.hand) == 0
+            and player_instant.uno
+            and player_instant.unoout
+        ):
             return True
 
         return False
@@ -309,7 +341,7 @@ class UNOGAMEMODEL:
             picked_card: An integer or string type representing the position of
                 the card in the players hand that they want to play in the game (if an integer)
                 or the desire to pick a card for their turn (if a string)
-            personal_hand:a list representing the player's hand who turn it is
+            personal_hand: A list representing the human player's hand
 
         """
 
@@ -324,29 +356,32 @@ class UNOGAMEMODEL:
 
     def computer_player_turn(self):
         """
-        Runs the virtual 'computer' player's turn
+        Runs the virtual 'computer' player's turn. Performs the artificial player's (computer's)
+        turn by having them choose what card they will play or if they will pick a card, and then
+        performing their chosen action in the game.
 
         """
 
-        self.deck.computer_deck.computer_choice(self.deck.main_deck, self.computer_hand)
+        for card_pos in enumerate(self.computer_hand):
+
+            if self.is_card_valid_move(self.computer_hand[card_pos]):
+
+                self.deck.played_cards.append(self.computer_hand[card_pos])
+                self.computer_hand.pop(card_pos)
+                break
+
+            if card_pos == (
+                len(self.computer_hand) - 1
+            ) and not self.is_card_valid_move(self.computer_hand[card_pos]):
+                self.pick_a_card(self.computer_hand)
+
+        if self.computer_turn is True and len(self.computer_hand) == 1:
+            self.deck.computer_deck.uno = True
+
+        if self.computer_turn is True and len(self.computer_hand) == 0:
+            self.deck.computer_deck.unoout = True
 
         self._flip_next_move()
-
-    def refill_deck(self):
-        """
-        Refills and reshuffles the main deck when it runs out during an UNO game by taking all
-        but the top played cards from the played card deck, shuffling them, and making them the
-        new main deck
-
-        """
-
-        if len(self.deck.main_deck) == 0:
-            length_of_played_deck = len(self.deck.played_cards)
-            temp_list = self.deck.played_cards[0 : length_of_played_deck - 1]
-            self.deck.played_cards = self.deck.played_cards[-1]
-
-            shuffle(temp_list)
-            self.deck.main_deck = temp_list
 
     def countdown(self, seconds):
         """
@@ -368,41 +403,15 @@ class UNOGAMEMODEL:
 
     def call_uno(self):
         """
-        Checks that once the human player has only 1 card left, they type in Uno within a minute to
-        claim UNO before their opponet does, and establishes that once the virtual player has only
-        1 card left they claim UNO as well
+        Declares the human player has called out UNO
 
         """
-        if self.computer_turn is True and len(self.computer_hand) == 1:
-            self.computer_uno = True
-
-        else:
-            self.computer_uno = False
-
-        if self.player_turn is True and len(self.player_hand) == 1:
-            while self.countdown(60) != "Time's up!":
-                call_uno = input("What do you say?")
-                if call_uno == ("Uno"):
-                    self.player_uno = True
-            self.player_uno = False
+        self.deck.human_deck.uno = True
 
     def call_uno_out(self):
         """
-        Checks that once the human player has no cards left, they type in Uno Out within a minute to
-        claim UNO OUT before their opponet does, and establishes that once the virtual player has no
-        cards left they claim UNO OUT as well
+        Declares the human player has called out UNO OUT
 
         """
 
-        if self.computer_turn is True and len(self.computer_hand) == 0:
-            self.computer_unoout = True
-
-        else:
-            self.computer_unoout = False
-
-        if self.player_turn is True and len(self.player_hand) == 0:
-            while self.countdown(60) != "Time's up!":
-                call_uno = input("What do you say?")
-                if call_uno == ("Uno Out"):
-                    self.player_unoout = True
-            self.player_unoout = False
+        self.deck.human_deck.unoout = True
