602c36bc4b0d8e4959b7b7c3b786473f1f144594
alex_zigs <azeigler@olin.edu>
2025-05-05 21:14:31 -0400

    debugging controller

--- uno_controller.py
+++ uno_controller.py
@@ -9,19 +9,17 @@ from pygame.locals import *
 mainClock = pygame.time.Clock()
 pygame.init()
 
-clicking = False
+CLICKING = False
 
-class MouseController:
+class MouseController():
     """
         Tell when the mouse is clicking on a button or a carrd
     """
-    
 
-    def __init__(self, model, rect):
+    def __init__(self, model):
 
         self._model = model
         self.clicked = False
-        self.rect = rect
         self.mouse_x, self.mouse_y = pygame.mouse.get_pos()
 
 
